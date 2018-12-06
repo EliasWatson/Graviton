78f19cbe3523de154e1bf10da3b82fdbd26d45f9
ewats176 <ewats176@students.bju.edu>
2018-12-05 21:21:11 -0500

    Fixed bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -39,9 +39,10 @@ main:
 
 	; TODO: use BIOS raw disk I/O to load sector 2 from disk number <boot_disk> into memory at 0800:0000h (retry on failure)
     .load_disk:
-    mov ah, 0x2
-	mov al, 0x4
+    mov ax, 0x800
     mov es, ax
+	mov ah, 0x2
+	mov al, 0x4
     mov cx, 0x2
     xor dx, dx
     mov dl, [boot_disk]
