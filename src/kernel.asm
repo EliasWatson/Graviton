4a40eb5f77d6f6a2d93ce616e9feab875ad0579c
Magnawarp the Great <bviss445@students.bju.edu>
2018-12-05 21:19:14 -0500

    compile successful

--- src/kernel.asm
+++ src/kernel.asm
@@ -9,53 +9,6 @@ IVT8_SEGMENT_SLOT	equ	IVT8_OFFSET_SLOT + 2	; Segment after Offset
 IVT9_OFFSET_SLOT	equ	4 * 9
 IVT9_SEGMENT_SLOT	equ	IVT9_OFFSET_SLOT + 2
 
-;all images are 16x16 pixels
-
-;creates an image struc with max 16 pixels
-;struc wordImage
-	;the coordinates (on the dosbox display) of the upper left corner of the image
-;    .x_coord: resw 0
-;    .y_coord: resb 0
-    ;the position of each pixel in the image relative to the top left corner. For each byte: lower half: x, higher half: y
-;    .posMap: TIMES 2 resq 0
-	;each byte is the color of the pixel in the same byte of posMap
-;	.colMap:	TIMES 2 resq 0
-;    .size:
-;endstruc
-
-;creates an image struc with max 32 pixels
-struc quadImage
-	;the coordinates (on the dosbox display) of the upper left corner of the image
-    .x_coord: resw 0
-    .y_coord: resb 0
-    ;the position of each pixel in the image relative to the top left corner. For each byte: lower half: x, higher half: y
-    .posMap: TIMES 4 resq 0
-	;each byte is the color of the pixel in the same byte of posMap
-	.colMap:	TIMES 4 resq 0
-    .size:
-endstruc
-
-;creates an image struc with max 64 pixels
-;struc octImage
-	;the coordinates (on the dosbox display) of the upper left corner of the image
-;    .x_coord: resw 0
-;    .y_coord: resb 0
-    ;the position of each pixel in the image relative to the top left corner. For each byte: lower half: x, higher half: y
-;    .posMap: TIMES 8 resq 0
-	;each byte is the color of the pixel in the same byte of posMap
-;	.colMap:	TIMES 8 resq 0
-;    .size:
-;endstruc
-
-;creates a gravity well struc
-struc grav_well
-	;the coordinates (on the dosbox display) of the upper left corner of the well
-    .x_coord: resw 0
-    .y_coord: resb 0
-	;denotes the current energy level of the well (can only go to 10 before resetting)
-	.power_level: resb 0
-    .size:
-endstruc
 
 SECTION .text
 main:
@@ -203,20 +156,7 @@ sustain_wells:
 
 ;well graphics thread
 render_wells:
-	mov bx, [grav_one]
-	mov ax, word [bx]
-	mov cx, [bx + 2]
-	mov bx, [grav_sprite]
-	mov [bx], ax
-	mov [bx + 2], cx
 .loop_forever_4:
-	mov bx, [grav_sprite]
-	call mutex
-	call displayQuadImage
-	call release
-	jmp     .loop_forever_4
-	; does not terminate or return
-
 	;not entirely sure where to put this
 	mov ax, 0x800
 	mov ds, ax
@@ -254,92 +194,6 @@ timer_isr:
 	; Chain (i.e., jump) to the original INT 8 handler 
 	jmp	far [cs:ivt8_offset]	; Use CS as the segment here, since who knows what DS is now
 
-;assumes vga mode is already set and es is set to A0000
-;accepts the memory address of an image struc in bx as a parameter
-global displayQuadImage
-displayQuadImage:
-	;save all registers
-	pusha
-	;set allPurposeCounter to 0
-	mov dword [allPurposeCounter], 0
-	;mov the address of the colMap and posMap into regs
-	lea dx, [bx + quadImage.colMap]
-	lea cx, [bx + quadImage.posMap]
-;loop through the posmap until the address equals the address of the colmap
-.loopPix:
-	;check if the current posMap address is equal to the starting address of colMap
-	cmp cx, dx
-	;if yes, jump out of loop
-	je .completePix
-	;get the posmap byte
-	mov bx, cx
-	mov bx, [bx]
-	;mov upper half into ax and multiply by 320
-	movzx ax, bh
-	mov bp, 320
-	mul bp
-	;add lower half of coordinate reg (x-value) and mov it into bp
-	movzx bx, bl
-	add bx, ax
-	mov bp, [bx]
-	;get color byte
-	mov bx, dx
-	mov bx, [bx]
-	add bx, [allPurposeCounter]
-	mov ax, [bx]
-	;display to screen
-	mov     [es:bp], ax
-	;increase memory address for cx and increment counter
-	add cx, 8
-	add dword [allPurposeCounter], 8
-	jmp .loopPix
-.completePix:
-	ret
-
-; takes a char to print in dx
-; no return value
-; putchar:
-; 	mov     ax, dx          ; call interrupt x10 sub interrupt xE
-; 	mov     ah, 0x0E
-; 	mov     cx, 1
-; 	int     0x10
-; 	ret
-
-;takes an address to write to in di
-;writes to address until a newline is encountered
-;returns nothing
-; putstring:
-; 	cmp     byte [di], 0        ; see if the current byte is a null terminator
-; 	je     	.done 				; nope keep printing
-; .continue:
-; 	mov     dl, [di]            ; grab the next character of the string
-; 	mov     dh, 0               ; print it
-; 	call    putchar
-; 	inc     di                  ; move to the next character
-; 	jmp     putstring
-; .done:
-; 	ret
-
-mutex:
-    push    ax
-    mov     ax, 0
-    jmp     .try
-.loop:
-    int     0x8
-.try:
-    xchg    [mutex_v], ax                        ; when ax is 1, we got the lock so move one
-    cmp     ax, 0
-    je      .loop                                ; loop while ax is 0, we didn't get the lock
-    pop     ax
-    ret
-
-release:
-    push    ax
-    mov     ax, 1
-    xchg    [mutex_v], ax                        ; set mutex_v back to 1 so someone else can grab the lock
-    pop     ax
-    int     0x8
-    ret
 
 SECTION .data
 	rect_a_x: dw 0
@@ -354,46 +208,6 @@ SECTION .data
 
 	ivt9_offset	 dw	0
 	ivt9_segment dw	0
-
-	allPurposeCounter: dq 0
-
-	;create six empty well strucs at six positions
-	grav_one:
-        dw 0x64
-        db 0x32
-        db 0
-    grav_two:
-        dw 0xC8
-        db 0x32
-        db 0
-    grav_three:
-        dw 0x96
-        db 0x64
-        db 0
-    grav_four:
-        dw 0xFA
-        db 0x64
-        db 0
-    grav_five:
-        dw 0x64
-        db 0x96
-        db 0
-    grav_six:
-        dw 0xC8
-        db 0x96
-        db 0
-
-	;gravity well sprites
-	grav_sprite:
-		dw 0
-		db 0
-		TIMES 4 dq 0x9537CA
-		TIMES 4 dq 0x0B0B0B
-
-	;energy sprites
-	;quadImage
-
-	mutex_v: dw 1
 
 	current_task: dw 0 ; must always be a multiple of 2
 	stacks: times (256 * 4) db 0 ; 31 fake stacks of size 256 bytes
